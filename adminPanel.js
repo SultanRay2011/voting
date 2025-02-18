7557bc59b4c9e6e081ee9188bb2b1d2a50f65b69
SultanRay2011 <ssalakomy8@gmail.com>
2025-02-19 00:51:37 +0100

    Update adminPanel.js

--- adminPanel.js
+++ adminPanel.js
@@ -1,4 +1,7 @@
 function clearVotes() {
     localStorage.removeItem('votes'); // Clear the stored votes
     document.getElementById('clearMessage').innerText = "Votes have been cleared!";
+    
+    // Show the 'Go Back to Home' button after clearing votes
+    document.getElementById('homeButton').style.display = 'block';
 }
